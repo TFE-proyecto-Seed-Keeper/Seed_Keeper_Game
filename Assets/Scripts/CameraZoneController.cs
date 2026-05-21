7e5d03241321890b558bdb92b65c73df06e9f2a4
Diego Fabian Salamanca Garcia <diegocolmayor@gmail.com>
2026-05-20 23:57:22 -0500

    pruebas nivel 1

---
+++ Assets/Scripts/CameraZoneController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using  Unity.Cinemachine;
+
+public class CameraZoneController : MonoBehaviour
+{
+
+    public CinemachineCamera thisCamera;
+    public float distance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ResetCameras();
+            thisCamera.Priority = 1;
+            thisCamera.GetComponent<CinemachinePositionComposer>().CameraDistance = distance;
+        }
+    }
+
+    void ResetCameras()
+    {
+        var cameras = FindObjectsOfType<CinemachineCamera>();
+        foreach (var cam in cameras)   
+        {
+            cam.Priority = 0;
+        }
+    }
+}
